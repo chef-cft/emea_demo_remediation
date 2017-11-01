8a4595bcd3eb4156e0f675ed7018d18aac05dc56
chef <chef@chef.io>
2017-11-01 14:46:35 +0000

    Added entropy recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,7 @@
 case node['os']
 when 'linux'
   include_recipe 'os-hardening::default'
+  include_recipe 'emea_demo_remediation::entropy'
 when 'windows'
   include_recipe 'windows-hardening::default'
 else
